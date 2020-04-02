f2bbb7071b82162176b8b32638ce35fa2e384c0e
Hugo <hmerle14@gmail.com>
2020-04-02 11:43:24 +0200

    Correction of printing bugs

--- form2.php
+++ form2.php
@@ -1,7 +1,7 @@
 <?php
 
 class form2 extends formulaire{
-    public function addRadioButton($name = "", $id){
+    final public function addRadioButton($name = "", $id){
         echo "<input type='radio' id=".$id." name=".$id." value=".$name.">
               <label for=".$name.">$name</label><br>";
     }
--- main.php
+++ main.php
@@ -8,11 +8,11 @@ include 'form2.php';
 $form = new form2("post", "main.php");
 $form->ajouterZoneDeTexte("Nom");
 $form->ajouterZoneDeTexte("Code");
-$form->ajouterBouton();
 $form->addRadioButton('Homme', "id");
 $form->addRadioButton('Femme', "id");
 $form->addCheckBox('Tennis');
 $form->addCheckBox('Archerie');
+$form->ajouterBouton();
 $form->getForm();
 
 if(isset($_POST["Nom"]) && isset($_POST["Code"])) {
@@ -20,7 +20,7 @@ if(isset($_POST["Nom"]) && isset($_POST["Code"])) {
     echo $_POST['Code']."<br>";
 }
 
-if(isset($_POST["Homme"]) || isset($_POST["Femme"])) {
+if(isset($_POST["id"])) {
     if(isset($_POST["Homme"])) {
         echo "C'est un homme.<br>";
     }
