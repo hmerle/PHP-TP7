a0b3f64e40067c6663e206ea5a0e60ea3fb2860c
Hugo <hmerle14@gmail.com>
2020-04-05 15:26:16 +0200

    Add class Square and Circle

--- ex2.php
+++ ex2.php
@@ -10,4 +10,25 @@ class Square implements Shape {
     function getArea(){
         return $this->height * $this->width;
     }
+    function __construct($h, $w){
+        $this->height = $h;
+        $this->width = $w;
+    }
+}
+
+class Circle implements Shape {
+    private $radius;
+    function getArea(){
+        return 3.14159 * $this->radius * $this->radius;
+    }
+    function __construct($r){
+        $this->radius = $r;
+    }
+}
+
+function testShapes(){
+    $s = new Square(5,5);
+    echo $s->getArea();
+    $c = new Circle(5);
+    echo $c->getArea();
 }
