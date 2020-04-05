4a8cca1f429178319e5e2871795fe1601ee15217
Hugo <hmerle14@gmail.com>
2020-04-05 15:49:29 +0200

    Add ex3

---
+++ ex3.php
@@ -0,0 +1,37 @@
+<?php
+
+trait UN{
+    public function small($text){
+        echo "<small>$text</small>";
+    }
+    public function big($text){
+        echo "<h4>$text</h4>";
+    }
+}
+
+trait DEUX{
+    public function small($text){
+        echo "<i>$text</i>";
+    }
+    public function big($text){
+        echo "<h2>$text</h2>";
+    }
+}
+
+class Texte{
+    private $text;
+    public function __construct($text){
+        $this->text = $text;
+    }
+
+    use UN, DEUX {
+        DEUX::small insteadof UN::small;
+        DEUX::small as petit;
+        UN::big insteadof DEUX::big;
+        UN::big as gros;
+    }
+}
+
+$text = new Texte("Ceci est le texte");
+$text->petit($text);
+$text->gros($text);
